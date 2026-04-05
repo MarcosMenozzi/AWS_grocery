ae5bcd758e0eba602796871e399decbb48bd4b1c
MarcosMenozzi <marcos.menozzi@outlook.de>
2026-04-05 13:47:11 +0200

    Add EC2 instance with Terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+}
