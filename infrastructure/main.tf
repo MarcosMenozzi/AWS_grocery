bd902c88ea09ff23f0628a45ab5b23628d13e8a1
MarcosMenozzi <marcos.menozzi@outlook.de>
2026-04-05 13:55:07 +0200

    Add security group and connect to EC2
    Added a security group with ingress and egress rules.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,4 +16,31 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+}
+
+resource "aws_security_group" "my_sg" {
+  name = "my-security-group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
